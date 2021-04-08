a4d032973ecc85550a52881017554974c9f24ce6
KimGeon <byn_purple@naver.com>
2021-04-08 10:55:59 +0900

    AspNetCore2017Ver

---
+++ Practice1/Controllers/MemberShipController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practice1.Models;
+
+namespace Practice1.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginInfo info)
+        {
+            String sId = "userId";
+            String sPassword = "userPass";
+            String msg = String.Empty;
+
+            if (ModelState.IsValid)
+            {
+                if (info.UserId.Equals(sId) && info.Password.Equals(sPassword))
+                {
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+                    return RedirectToAction("Index", "Membership");
+                }
+                else
+                {
+                    msg = "해당 아이디와 비밀번호가 일치하지 않습니다.";
+                }
+            }
+            else
+            {
+                msg = "로그인 정보가 올바르지 않습니다.";
+            }
+
+
+            ModelState.AddModelError(string.Empty, msg);
+            return View();
+        }
+    }
+}
---
+++ Practice1/Models/LoginInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice1.Models
+{
+    public class LoginInfo
+    {
+        [Required(ErrorMessage = "사용자 아이디를 입력하세요")]
+        [MinLength(6, ErrorMessage = "사용자 아이디는 6자 이상 입력")]
+        [Display(Name = "사용자 아이디")]
+        public String UserId { get; set; }
+
+        [Required(ErrorMessage = "비밀번호를 입력하세요.")]
+        [MinLength(6, ErrorMessage = "비밀번호는 6자 이상 입력")]
+        [Display(Name = "비밀번호")]
+        public String Password { get; set; }
+    }
+}
